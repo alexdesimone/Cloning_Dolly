107560a7cbe4d1d50433f21cee6eb4c46bf64a6f
Alex De Simone <ajdesimone@gmail.com>
2012-05-10 00:18:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloningDolly::Application.config.secret_token = 'ccb24ba349e80717574dc5ef4333facf66ed8a1e5a37deeb3785c24723b5ae284d51f422b70d6fee98d25eaa92e6abf92d4e754e28ae09c3a2ed2540105edde3'
